72c13ae605f753879f7dfae49901f6503edccda1
juliana <julianapereira.dev@gmail.com>
2023-07-12 09:40:44 -0300

    Build: react-router-dom e styled-components. Style: Componentes Reset e Global style

---
+++ src/style/GlobalStyle.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        font-family: 'Raleway';
+        font-style: normal;
+        font-weight: 400;
+    }
+`
+
+export default GlobalStyle
