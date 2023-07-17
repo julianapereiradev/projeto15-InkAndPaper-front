0ab0a6e599975f834035141be15adfc07d12b282
GDMeira <gabrieldega970@gmail.com>
2023-07-17 12:41:32 -0300

    refac: ajuste da responsividade das telas de sign-in e sign-up.

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -21,15 +21,15 @@ export default function SignUpPage() {
 
   async function handleSubmit(e) {
     e.preventDefault();
-    
+
     if (formStates.password !== formStates.checkPassword) {
       return alert('Confirmação de senha está incorreta!')
     }
-    
-    const newUser = {...formStates};
+
+    const newUser = { ...formStates };
     delete newUser.checkPassword;
     setDisable(true);
-    
+
     axios.post(requisitions.postSignUp, newUser)
       .then(() => {
         navigate(pages.signIn)
@@ -42,146 +42,146 @@ export default function SignUpPage() {
   }
 
   function handleChange(e) {
-    const newFormStates = {...formStates};
+    const newFormStates = { ...formStates };
     newFormStates[e.target.id] = e.target.value;
     setFormStates(newFormStates);
   }
 
   return (
     <SignUpContainer>
-    <SignUpBox>
-      <LeftBox>
-      <img src={welcomeImage} style={{maxHeight: '800px'}} />
-      </LeftBox>
-      
-      <RightBox>
-      <form onSubmit={e => handleSubmit(e)}>
-      <FormBox>
+      <SignUpBox>
+        <LeftBox bgImage={welcomeImage}>
+          {/* <img src={welcomeImage} /> */}
+        </LeftBox>
 
-        <TopBox >
-        <Logo />
-        <div className="font-brit">Cadastro</div>
-        </TopBox>
-        
-        <InputBox>
-        <input
-          id="name"
-          placeholder="Nome" 
-          type="text" 
-          value={formStates.name}
-          onChange={e => handleChange(e)}
-          autoComplete="name"
-          required
-          disabled={disable}
-        />
-        <input
-          id="email"
-          placeholder="E-mail" 
-          type="email"
-          autoComplete="username"
-          value={formStates.email}
-          onChange={e => handleChange(e)}
-          required
-          disabled={disable}
-        />
-        <input
-          id="password"
-          placeholder="Senha" 
-          type="password" 
-          autoComplete="new-password" 
-          value={formStates.password}
-          onChange={e => handleChange(e)}
-          required
-          disabled={disable}
-        />
-        <input 
-          id="checkPassword"
-          placeholder="Confirme a senha" 
-          type="password" 
-          autoComplete="new-password" 
-          value={formStates.checkPassword}
-          onChange={e => handleChange(e)}
-          required
-          disabled={disable}
-        />
-        </InputBox>
+        <RightBox>
+          <form onSubmit={e => handleSubmit(e)}>
+            <FormBox>
 
-        <BottomBox>
-        <button
-        type="submit" 
-        disabled={disable}
-        >
-          {disable ? (
-            <ThreeDots type="ThreeDots" color="#1F1712" height={20} width={50} />
-          ) : (
-            "Cadastrar"
-          )}
-        </button>
-        <LinkToSignIn to={pages.signIn}>
-        Já tem uma conta? Entre agora!
-      </LinkToSignIn>
-      </BottomBox>
+              <TopBox >
+                <Logo />
+                <div className="font-brit">Cadastro</div>
+              </TopBox>
 
-      </FormBox>
-      </form>
-      </RightBox>
-    </SignUpBox>
+              <InputBox>
+                <input
+                  id="name"
+                  placeholder="Nome"
+                  type="text"
+                  value={formStates.name}
+                  onChange={e => handleChange(e)}
+                  autoComplete="name"
+                  required
+                  disabled={disable}
+                />
+                <input
+                  id="email"
+                  placeholder="E-mail"
+                  type="email"
+                  autoComplete="username"
+                  value={formStates.email}
+                  onChange={e => handleChange(e)}
+                  required
+                  disabled={disable}
+                />
+                <input
+                  id="password"
+                  placeholder="Senha"
+                  type="password"
+                  autoComplete="new-password"
+                  value={formStates.password}
+                  onChange={e => handleChange(e)}
+                  required
+                  disabled={disable}
+                />
+                <input
+                  id="checkPassword"
+                  placeholder="Confirme a senha"
+                  type="password"
+                  autoComplete="new-password"
+                  value={formStates.checkPassword}
+                  onChange={e => handleChange(e)}
+                  required
+                  disabled={disable}
+                />
+              </InputBox>
+
+              <BottomBox>
+                <button
+                  type="submit"
+                  disabled={disable}
+                >
+                  {disable ? (
+                    <ThreeDots type="ThreeDots" color="#1F1712" height={20} width={50} />
+                  ) : (
+                    "Cadastrar"
+                  )}
+                </button>
+                <LinkToSignIn to={pages.signIn}>
+                  Já tem uma conta? Entre agora!
+                </LinkToSignIn>
+              </BottomBox>
+
+            </FormBox>
+          </form>
+        </RightBox>
+      </SignUpBox>
     </SignUpContainer>
   )
 }
 
 const SignUpContainer = styled.div`
-background-color: #1F1712;
-width: 100vw;
-height: 100vh;
-display: flex;
-justify-content: center;
-align-items: center;
+  background-color: #1F1712;
+  width: 100vw;
+  height: 100%;
+  display: flex;
 `
 
 const SignUpBox = styled.div`
-width: 100vw;
-height: 100vh;
+  width: 100vw;
+
 `
 
 const LeftBox = styled.div`
-width: 40vw;
-float: left;
-
-img {
-  height: 100vh;
-}
+  height: 100%;
+  display: flex;
+  min-height: 100vh;
+  width: 40vw;
+  float: left;
+  background-image: url(${props => props.bgImage});
+  background-size: cover;
 `
 const RightBox = styled.div`
-width: 60vw;
-height: 100%;
-float: right;
-display: flex;
-justify-content: center;
-align-items: center;
-background-color: #1F1712;
+  width: 60vw;
+  height: 100%;
+  float: right;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #1F1712;
 `
 
 const FormBox = styled.div`
-background-color: #1F1712;
-height: 100%;
-display: flex;
-flex-direction: column;
-justify-content: space-evenly;
+  background-color: #1F1712;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
 `
 
 const TopBox = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `
 
 const InputBox = styled.div`
-margin-top: 30px;
-display: flex;
-flex-direction: column;
-width: 400px;
-align-self: center;
+  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+  width: 400px;
+  align-self: center;
 
 input {
   margin-bottom: 30px;
@@ -202,8 +202,8 @@ input::placeholder {
 `
 
 const BottomBox = styled.div`
-display: flex;
-flex-direction: column;
+  display: flex;
+  flex-direction: column;
 
 button {
   background-color: #F6E4C4;
